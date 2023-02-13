7344ff39990dd1024513efe5353c21a47bbbab44
42bahn <bbu0704@gmail.com>
2023-02-13 18:13:52 +0900

    vue-cli

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
-
-
-const app = createApp({
-    data() {
-        return {
-            count: 0
-        }
-    }
-})
-
-// app.mount('#app')
 createApp(App).mount('#app')
