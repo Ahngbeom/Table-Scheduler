3e5be64ca9b49868cf3dbd1b5f8ee226843cc335
42bahn <bbu0704@gmail.com>
2023-02-13 22:12:50 +0900

    bootstrap-vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <MyTable />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import MyTable from './components/MyTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    /*HelloWorld, */MyTable
   }
 }
 </script>
